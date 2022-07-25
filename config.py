a594586f59c68315a1041af8e3be9d6a2e690341
Idoma Ngbede <idomakevin@gmail.com>
2022-07-25 14:41:38 +0100

    Reviewed and fixed some errors

--- config.py
+++ config.py
@@ -10,4 +10,7 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:idomason@localhost:5432/fyyur'
+
+# Block warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
